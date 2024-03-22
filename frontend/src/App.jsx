6cd342b80cdbb0ebcb6ff966c30dd8c18534206e
lee <lee_malachi@163.com>
2024-03-22 23:07:55 +0800

    install react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About'
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Project from './pages/Project';
+
 export default function App() {
   return (
-    <h1>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/project' element={<Project />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
